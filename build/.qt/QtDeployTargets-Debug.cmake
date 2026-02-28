9d87ef7cd123040f7ee204df40045a390e301c4d
NIVEDCHANDRAN <chandrannived7@gmail.com>
2026-02-28 15:56:46 +0530

    fiest commit

--- build/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_HOT_Control_App_FILE C:/Users/NIVED/Desktop/gui_c/HOT_Control_App/build/Debug/HOT_Control_App.exe)
-set(__QT_DEPLOY_TARGET_HOT_Control_App_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_HOT_Control_App_RUNTIME_DLLS C:/Qt/6.10.2/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Cored.dll)
